7ebc7ce0dab9d2439aeccc2d7703cd257b41a931
Sergey Laletin <Quercue@gmail.com>
2025-09-06 13:04:01 +0300

    add tailwind css

--- src/pages/ProductsPage/index.tsx
+++ src/pages/ProductsPage/index.tsx
@@ -22,6 +22,7 @@ export function ProductsPage() {
           <h2>{product.title}</h2>
           <p>{product.description}</p>
           <p>Price: ${product.price}</p>
+          <img src={product.image} alt={product.title} />
         </div>
       ))}
     </>
