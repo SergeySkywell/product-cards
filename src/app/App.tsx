f53af038767258c0e25c8a0a737e36e4657ccae4
Sergey Laletin <Quercue@gmail.com>
2025-09-08 12:25:44 +0300

    create AppRoutes component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,12 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
 import { Header } from "../widgets/Header";
-import { ProductsPage } from "../pages/ProductsPage";
-import { ProductPage } from "../pages/ProductPage";
-import { CreateProductPage } from "../pages/CreateProductPage";
+import { AppRoutes } from "./router/AppRoutes";
 
 function App() {
   return (
     <HashRouter>
       <Header />
-      <Routes>
-        <Route path="/products" element={<ProductsPage />} />
-        <Route path="/products/:id" element={<ProductPage />} />
-        <Route path="/create-product" element={<CreateProductPage />} />
-      </Routes>
+      <AppRoutes />
     </HashRouter>
   );
 }
---
+++ src/app/router/AppRoutes.tsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import { ProductsPage } from "../../pages/ProductsPage";
+import { ProductPage } from "../../pages/ProductPage";
+import { CreateProductPage } from "../../pages/CreateProductPage";
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/products" element={<ProductsPage />} />
+      <Route path="/products/:id" element={<ProductPage />} />
+      <Route path="/create-product" element={<CreateProductPage />} />
+    </Routes>
+  );
+};
